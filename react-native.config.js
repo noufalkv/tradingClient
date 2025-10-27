926b1fd5e0fccd58777ac67ca0bdae9571c4f24d
Noufal <noufal.vadakkyil@raknbank.ae>
2025-10-27 13:11:44 +0400

    Fix package name in package.json and package-lock.json; update react-native.config.js structure

--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,15 @@
+// const { Platform } = require("react-native");
+
 module.exports = {
+  project: {
+    ios: {},
+    android: {
+      packageName: 'com.rn_trading_app',
+    },
+  },
+  'react-native-vector-icons': {
+    Platforms: { ios: null },
+  },
   assets: ['./src/assets/fonts/'],
   getTransformModulePath() {
     return require.resolve('react-native-typescript-transformer');
